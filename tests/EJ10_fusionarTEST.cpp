fd895bf270e7ce4c0fe43dd9d0f76f4e2b51da38
Gabo <gabrielbacar@gmail.com>
2020-11-09 17:56:50 -0500

    Agregados tests.

--- tests/EJ10_fusionarTEST.cpp
+++ tests/EJ10_fusionarTEST.cpp
@@ -22,3 +22,41 @@ TEST(fusionarTEST, sinInterseccion){
     toroide res = fusionar(t1, t2);
     EXPECT_EQ(res, tout);
 }
+
+TEST(fusionarTEST, dosIguales){
+    toroide t1 = {
+            {true, false, false},
+            {false, true, false},
+            {false, false, true}};
+    toroide t2 = {
+            {true, false, false},
+            {false, true, false},
+            {false, false, true}};
+
+    toroide tout = {
+            {true, false, false},
+            {false, true, false},
+            {false, false, true}};
+
+    toroide res = fusionar(t1, t2);
+    EXPECT_EQ(res, tout);
+}
+
+TEST(fusionarTEST, unaInterseccion){
+    toroide t1 = {
+            {true, false, false},
+            {false, true, false},
+            {false, false, true}};
+    toroide t2 = {
+            {true, true, true},
+            {true, false, true},
+            {true, true, false}};
+
+    toroide tout = {
+            {true, false, false},
+            {false, false, false},
+            {false, false, false}};
+
+    toroide res = fusionar(t1, t2);
+    EXPECT_EQ(res, tout);
+}
